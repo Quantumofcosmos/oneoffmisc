49cd710eebcee7207f61b0baeb44474f5077f4d4
quantumofcosmos <quantumofcosmos@gmail.com>
2019-10-22 07:22:54 +0530

    added rayshader, changed fft

---
+++ rayshader/vizag.R
@@ -0,0 +1,37 @@
+# define bounding box with longitude/latitude coordinates
+bbox <- list(
+  p1 = list(long = 83, lat = 17),
+  p2 = list(long = 84, lat = 18)
+)
+
+leaflet() %>%
+  addTiles() %>% 
+  addRectangles(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat,
+    fillColor = "transparent"
+  ) %>%
+  fitBounds(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat,
+  )
+
+# load elevation data
+elev_img <- raster::raster("cdne44r.tif")
+elev_matrix <- matrix(
+  raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
+  nrow = ncol(elev_img), ncol = nrow(elev_img)
+)
+
+# calculate rayshader layers
+ambmat <- ambient_shade(elev_matrix, zscale = 30)
+raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
+watermap <- detect_water(elev_matrix)
+
+# plot 2D
+elev_matrix %>%
+  sphere_shade(texture = "imhof4") %>%
+  add_water(watermap, color = "imhof4") %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  plot_map()
--- sonicviz/fft.py
+++ sonicviz/fft.py
@@ -17,6 +17,9 @@ import IPython as ipy
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
+import time
+
+start_time = time.time()
 
 # Audio Format (check Audio MIDI Setup if on Mac)
 FORMAT = pyaudio.paInt16
@@ -24,111 +27,118 @@ RATE = 44100
 CHANNELS = 2
 
 # Set Plot Range [-RANGE,RANGE], default is nyquist/2
-RANGE = 1000
+RANGE = 0
 if not RANGE:
-	RANGE = RATE/2
+    RANGE = RATE/2
 
 # Set these parameters (How much data to plot per FFT)
-INPUT_BLOCK_TIME = 0.05
+INPUT_BLOCK_TIME = 0.5
 INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
 
 # Which Channel? (L or R)
 LR = "l"
 
 class SpectrumAnalyzer():
-	def __init__(self):
-		self.pa = pyaudio.PyAudio()
-		self.initMicrophone()
-		self.initUI()
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+        self.initMicrophone()
+        self.initUI()
 
-	def find_input_device(self):
-		device_index = None            
-		for i in range(self.pa.get_device_count()):     
-		    devinfo = self.pa.get_device_info_by_index(i)   
-		    if devinfo["name"].lower() in ["mic","input"]:
-				device_index = i
+    def find_input_device(self):
+        device_index = None
+        for i in range(self.pa.get_device_count()):
+            devinfo = self.pa.get_device_info_by_index(i)
+            if devinfo["name"].lower() in ["mic","input"]:
+                device_index = i
 
-		return device_index
+        return device_index
 
-	def initMicrophone(self):
-		device_index = self.find_input_device()
+    def initMicrophone(self):
+        device_index = self.find_input_device()
 
-		self.stream = self.pa.open(	format = FORMAT,
-									channels = CHANNELS,
-									rate = RATE,
-									input = True,
-									input_device_index = device_index,
-									frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+        self.stream = self.pa.open(    format = FORMAT,
+                                    channels = CHANNELS,
+                                    rate = RATE,
+                                    input = True,
+                                    input_device_index = device_index,
+                                    frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
 
-	def readData(self):
-		block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
-		count = len(block)/2
-		format = "%dh"%(count)
-		shorts = struct.unpack( format, block )
-		if CHANNELS == 1:
-			return np.array(shorts)
-		else:
-			l = shorts[::2]
-			r = shorts[1::2]
-			if LR == 'l':
-				return np.array(l)
-			else:
-				return np.array(r)
+    def readData(self):
+        block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+        count = len(block)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, block )
+        if CHANNELS == 1:
+            return np.array(shorts)
+        else:
+            l = shorts[::2]
+            r = shorts[1::2]
+            if LR == 'l':
+                return np.array(l)
+            else:
+                return np.array(r)
 
-	def initUI(self):
-		self.app = QtGui.QApplication([])
-		self.app.quitOnLastWindowClosed()
+    def initUI(self):
+        self.app = QtGui.QApplication([])
+        self.app.quitOnLastWindowClosed()
 
-		self.mainWindow = QtGui.QMainWindow()
-		self.mainWindow.setWindowTitle("Spectrum Analyzer")
-		self.mainWindow.resize(800,300)
-		self.centralWid = QtGui.QWidget()
-		self.mainWindow.setCentralWidget(self.centralWid)
-		self.lay = QtGui.QVBoxLayout()
-		self.centralWid.setLayout(self.lay)
+        self.mainWindow = QtGui.QMainWindow()
+        self.mainWindow.setWindowTitle("Spectrum Analyzer")
+        self.mainWindow.resize(800,300)
+        self.centralWid = QtGui.QWidget()
+        self.mainWindow.setCentralWidget(self.centralWid)
+        self.lay = QtGui.QVBoxLayout()
+        self.centralWid.setLayout(self.lay)
 
-		self.specWid = pg.PlotWidget(name="spectrum")
-		self.specItem = self.specWid.getPlotItem()
-		self.specItem.setMouseEnabled(y=False)
-		self.specItem.setYRange(0,1000)
-		#self.specItem.setXRange(-RANGE,RANGE, padding=0)
-		self.specItem.setXRange(0,1000, padding=0)
+        self.specWid = pg.PlotWidget(name="spectrum")
+        self.specItem = self.specWid.getPlotItem()
+        self.specItem.setMouseEnabled(y=False)
+        self.specItem.setYRange(0,RANGE)
+        #self.specItem.setXRange(-RANGE,RANGE, padding=0)
+        self.specItem.setXRange(0,1000, padding=0)
 
-		self.specAxis = self.specItem.getAxis("bottom")
-		self.specAxis.setLabel("Frequency [Hz]")
-		self.lay.addWidget(self.specWid)
+        self.specAxis = self.specItem.getAxis("bottom")
+        self.specAxis.setLabel("Frequency [Hz]")
+        self.lay.addWidget(self.specWid)
 
-		self.mainWindow.show()
-		self.app.aboutToQuit.connect(self.close)
+        self.mainWindow.show()
+        self.app.aboutToQuit.connect(self.close)
 
-	def close(self):
-		self.stream.close()
-		sys.exit()
+    def close(self):
+        self.stream.close()
+        sys.exit()
 
-	def get_spectrum(self, data):
-		T = 1.0/RATE
-		N = data.shape[0]
-		Pxx = (1./N)*np.fft.fft(data)
-		f = np.fft.fftfreq(N,T)
-		Pxx = np.fft.fftshift(Pxx)
-		f = np.fft.fftshift(f)
+    def get_spectrum(self, data):
+        T = 1.0/RATE
+        N = data.shape[0]
+        Pxx = (1./N)*np.fft.fft(data)
+        f = np.fft.fftfreq(N,T)
+        Pxx = np.fft.fftshift(Pxx)
+        f = np.fft.fftshift(f)
 
-		return f.tolist(), (np.absolute(Pxx)).tolist()
+        return f.tolist(), (np.absolute(Pxx)).tolist()
 
-	def mainLoop(self):
-		while 1:
-			# Sometimes Input overflowed because of mouse events, ignore this
-			try:
-				data = self.readData()
-			except IOError:
-				continue
-			f, Pxx = self.get_spectrum(data)
-                        print(f)
-                        print(Pxx)
-                        break
-			self.specItem.plot(x=f,y=Pxx, clear=True)
-			QtGui.QApplication.processEvents()
+    def mainLoop(self):
+        while 1:
+            # Sometimes Input overflowed because of mouse events, ignore this
+            try:
+                data = self.readData()
+            except IOError:
+                continue
+            f, Pxx = self.get_spectrum(data)
+            global start_time
+            print(time.time() - start_time)
+            start_time = time.time()
+            # print(zip(f,Pxx))
+            #asd = Pxx[22050:23050]
+            #print(asd)
+            #print(asd.index(max(asd)))
+            # print(list((k,asd[k]) for k in asd.keys() & range(0,5000)))
+            #print(Pxx)
+            #break
+            self.specItem.plot(x=f,y=Pxx, clear=True)
+            QtGui.QApplication.processEvents()
 
 if __name__ == '__main__':
-	sa = SpectrumAnalyzer()
-	sa.mainLoop()
+    sa = SpectrumAnalyzer()
+    sa.mainLoop()
