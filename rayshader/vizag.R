cbe9d024ac63c0daaef16ec244d94386b01f763d
Venkatesh Yandapalli <yandapallivenkatesh94@gmail.com>
2019-10-28 17:49:36 +0530

    Update vizag.R with plot3d and ffmpeg command

--- rayshader/vizag.R
+++ rayshader/vizag.R
@@ -1,70 +1,8 @@
 library(tidyverse)
 library(rayshader)
 library(leaflet)
-get_arcgis_map_image <- function(bbox, map_type = "World_Street_Map", file = NULL, 
-                                 width = 400, height = 400, sr_bbox = 4326) {
-  require(httr)
-  require(glue) 
-  require(jsonlite)
-  
-  url <- parse_url("https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task/execute")
-  
-  # define JSON query parameter
-  web_map_param <- list(
-    baseMap = list(
-      baseMapLayers = list(
-        list(url = jsonlite::unbox(glue("https://services.arcgisonline.com/ArcGIS/rest/services/{map_type}/MapServer",
-                                        map_type = map_type)))
-      )
-    ),
-    exportOptions = list(
-      outputSize = c(width, height)
-    ),
-    mapOptions = list(
-      extent = list(
-        spatialReference = list(wkid = jsonlite::unbox(sr_bbox)),
-        xmax = jsonlite::unbox(max(bbox$p1$long, bbox$p2$long)),
-        xmin = jsonlite::unbox(min(bbox$p1$long, bbox$p2$long)),
-        ymax = jsonlite::unbox(max(bbox$p1$lat, bbox$p2$lat)),
-        ymin = jsonlite::unbox(min(bbox$p1$lat, bbox$p2$lat))
-      )
-    )
-  )
-  
-  res <- GET(
-    url, 
-    query = list(
-      f = "json",
-      Format = "PNG32",
-      Layout_Template = "MAP_ONLY",
-      Web_Map_as_JSON = jsonlite::toJSON(web_map_param))
-  )
-  
-  if (status_code(res) == 200) {
-    body <- content(res, type = "application/json")
-    message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
-    if (is.null(file)) 
-      file <- tempfile("overlay_img", fileext = ".png")
-    
-    img_res <- GET(body$results[[1]]$value$url)
-    img_bin <- content(img_res, "raw")
-    writeBin(img_bin, file)
-    message(paste("image saved to file:", file))
-  } else {
-    message(res)
-  }
-  invisible(file)
-}
-
-define_image_size <- function(bbox, major_dim = 400) {
-  # calculate aspect ration (width/height) from lat/long bounding box
-  aspect_ratio <- abs((bbox$p1$long - bbox$p2$long) / (bbox$p1$lat - bbox$p2$lat))
-  # define dimensions
-  img_width <- ifelse(aspect_ratio > 1, major_dim, major_dim*aspect_ratio) %>% round()
-  img_height <- ifelse(aspect_ratio < 1, major_dim, major_dim/aspect_ratio) %>% round()
-  size_str <- paste(img_width, img_height, sep = ",")
-  list(height = img_height, width = img_width, size = size_str)
-}
+library(raster)
+library(animation)
 
 # define bounding box with longitude/latitude coordinates
 bbox <- list(
@@ -85,19 +23,15 @@ leaflet() %>%
   )
 
 # load elevation data
-elev_img_fs <- raster::raster("cdne44r.tif")
-extnt <- extent(82.99986,83.5,17.5,18.00014)
-elev_img <- crop(elev_img_fs,extent(82.99986,83.5,17.5,18.00014))
+# elev_img_fs <- 
+elev_img <- crop(raster::raster("cdne44r.tif"),extent(82.99986,83.5,17.5,18.00014))
 
 elev_matrix <- matrix(
   raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
   nrow = ncol(elev_img), ncol = nrow(elev_img)
 )
 
-# calculate rayshader layers
-ambmat <- ambient_shade(elev_matrix, zscale = 30)
-raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
-watermap <- detect_water(elev_matrix)
+
 
 # plot 2D
 elev_matrix %>%
@@ -113,6 +47,12 @@ get_arcgis_map_image(bbox, map_type = "World_Topo_Map", file = overlay_file,
                      sr_bbox = 4326)
 overlay_img <- png::readPNG(overlay_file)
 
+resized_overlay_file = paste0(tempfile(),".png")
+grDevices::png(filename = resized_overlay_file, width = dim(elev_matrix)[1], height = dim(elev_matrix)[2])
+par(mar = c(0,0,0,0))
+plot(as.raster(overlay_img))
+dev.off()
+overlay_img = png::readPNG(resized_overlay_file)
 
 elev_matrix %>%
   sphere_shade(texture = "imhof4") %>%
@@ -124,113 +64,60 @@ elev_matrix %>%
 
 zscale <- 10
 rgl::clear3d()
-elev_matrix %>% 
-  sphere_shade(texture = "imhof4") %>% 
-  add_water(watermap, color = "imhof4") %>%
+# calculate rayshader layers
+ambmat <- ambient_shade(elev_matrix1, zscale = zscale)
+raymat <- ray_shade(elev_matrix1, zscale = zscale, lambert = TRUE)
+watermap <- detect_water(elev_matrix1)
+for (i in 0:29) {
+rgl::clear3d()
+elev_matrix1 %>% 
+  sphere_shade(texture = "imhof2") %>% 
+  add_water(watermap, color = "imhof2") %>%
   add_overlay(overlay_img, alphalayer = 0.5) %>%
   add_shadow(raymat, max_darken = 0.5) %>%
   add_shadow(ambmat, max_darken = 0.5) %>%
-  plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
-          water = TRUE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
-          theta = 25, phi = 30, zoom = 0.65, fov = 60)
-render_snapshot()
+  plot_3d(elev_matrix1, zscale = zscale, windowsize = c(1200, 1000),watercolor = "imhof2",
+          water = TRUE, wateralpha = 0.6,soliddepth = -60,waterdepth = 20+i,
+          theta = 0, phi = 45, zoom = 0.65, fov = 60)  
+render_snapshot(paste0("Test",i,".png",sep=""))
+}
 
+
+# You can do like this (assuming the images are in the current directory):
+
+imgs <- list.files(pattern="*.png")
+saveVideo({
+  for(img in 0:29){
+    im <- magick::image_read(paste0(img,".png",sep=""))
+    plot(as.raster(im))
+  }  
+},video.name = "highrate.mp4")
+paste0("Test",i,".png",sep="")
+# plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
+#         water = TRUE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
+#         theta = 45, phi = 45, zoom = 0.65, fov = 60)
 montereybay=elev_matrix
-
-transition_values <- function(from, to, steps = 10, 
-                              one_way = FALSE, type = "cos") {
-  if (!(type %in% c("cos", "lin")))
-    stop("type must be one of: 'cos', 'lin'")
-  
-  range <- c(from, to)
-  middle <- mean(range)
-  half_width <- diff(range)/2
-  
-  # define scaling vector starting at 1 (between 1 to -1)
-  if (type == "cos") {
-    scaling <- cos(seq(0, 2*pi / ifelse(one_way, 2, 1), length.out = steps))
-  } else if (type == "lin") {
-    if (one_way) {
-      xout <- seq(1, -1, length.out = steps)
-    } else {
-      xout <- c(seq(1, -1, length.out = floor(steps/2)), 
-                seq(-1, 1, length.out = ceiling(steps/2)))
-    }
-    scaling <- approx(x = c(-1, 1), y = c(-1, 1), xout = xout)$y 
-  }
-  
-  middle - half_width * scaling
-}
-
-save_3d_gif <- function(hillshade, heightmap, file, duration = 5, ...) {
-  require(rayshader)
-  require(magick)
-  require(rgl)
-  require(gifski)
-  require(rlang)
-  
-  # capture dot arguments and extract variables with length > 1 for gif frames
-  dots <- rlang::list2(...)
-  var_exception_list <- c("windowsize")
-  dot_var_lengths <- purrr::map_int(dots, length)
-  gif_var_names <- names(dots)[dot_var_lengths > 1 & 
-                                 !(names(dots) %in% var_exception_list)]
-  # split off dot variables to use on gif frames
-  gif_dots <- dots[gif_var_names]
-  static_dots <- dots[!(names(dots) %in% gif_var_names)]
-  gif_var_lengths <- purrr::map_int(gif_dots, length)
-  # build expressions for gif variables that include index 'i' (to use in the for loop)
-  gif_expr_list <- purrr::map(names(gif_dots), ~rlang::expr(gif_dots[[!!.x]][i]))
-  gif_exprs <- exprs(!!!gif_expr_list)
-  names(gif_exprs) <- names(gif_dots)
-  message(paste("gif variables found:", paste(names(gif_dots), collapse = ", ")))
-  
-  # TODO - can we recycle short vectors?
-  if (length(unique(gif_var_lengths)) > 1) 
-    stop("all gif input vectors must be the same length")
-  n_frames <- unique(gif_var_lengths)
-  
-  # generate temp .png images
-  temp_dir <- tempdir()
-  img_frames <- file.path(temp_dir, paste0("frame-", seq_len(n_frames), ".png"))
-  on.exit(unlink(img_frames))
-  message(paste("Generating", n_frames, "temporary .png images..."))
-  for (i in seq_len(n_frames)) {
-    message(paste(" - image", i, "of", n_frames))
-    rgl::clear3d()
-    hillshade %>%
-      plot_3d_tidy_eval(heightmap, !!!append(gif_exprs, static_dots))
-    rgl::snapshot3d(img_frames[i])
-  }
-  
-  # build gif
-  message("Generating .gif...")
-  magick::image_write_gif(magick::image_read(img_frames), 
-                          path = file, delay = duration/n_frames)
-  message("Done!")
-  invisible(file)
-}
-plot_3d_tidy_eval <- function(hillshade, ...) {
-  dots <- rlang::enquos(...)
-  plot_3d_call <- rlang::expr(plot_3d(hillshade, !!!dots))
-  rlang::eval_tidy(plot_3d_call)
-}
-
-n_frames <- 5
+median(elev_matrix)
+elev_matrix1 <- elev_matrix + 88
+elev_matrix1[elev_matrix1<0]=0
+n_frames <- 15
 waterdepths <- transition_values(from = 0, to = min(montereybay), steps = n_frames) 
 thetas <- transition_values(from = -45, to = -135, steps = n_frames)
 # generate gif
-zscale <- 50
+zscale <- 10
 montereybay %>% 
   sphere_shade(texture = "imhof1", zscale = zscale) %>%
   add_shadow(ambient_shade(montereybay, zscale = zscale), 0.5) %>%
   add_shadow(ray_shade(montereybay, zscale = zscale, lambert = TRUE), 0.5) %>%
+  plot_
+  render_movie("test1",theta = -45, phi = 45)
+  
   save_3d_gif(montereybay, file = "montereybay.gif", duration = 6,
               solid = TRUE, shadow = TRUE, water = TRUE, zscale = zscale,
               watercolor = "imhof3", wateralpha = 0.8, 
               waterlinecolor = "#ffffff", waterlinealpha = 0.5,
               waterdepth = waterdepths/zscale, 
-              theta = thetas, phi = 45)
+              theta = -45, phi = 45)
 
 
 raster::extent(elev_img)
@@ -243,3 +130,15 @@ montereybay %>%
   plot_3d(montereybay, zscale = 50, fov = 0, theta = -45, phi = 45, windowsize = c(1000, 800), zoom = 0.75,
           water = TRUE, waterdepth = 0, wateralpha = 0.5, watercolor = "lightblue",
           waterlinecolor = "white", waterlinealpha = 0.5)
+
+
+# ffmpeg -framerate 1 -i image%02d.png -s:v 1280x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -r 3 clip.mp4
+  
+  # input frame rate
+  # image names (image000.png, ..., image999.png)
+  # video size
+  # encoder
+  # H.264 profile for video
+  # constant rate factor
+  # pixel format
+  # output frame rate
